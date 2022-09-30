d34d8831624fc0de56ff19ac886667b8e55962b7
flaw03 <100543813+flaw03@users.noreply.github.com>
2022-09-30 11:25:48 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,34 +4,40 @@ public class Gaulois {
 	
 	private String nom;
 	private int force;
-	private int effetPotion = 1 ;
+	private int effetPotion = 1;
 	
-	public Gaulois(String nom, int force) {
-		super();
-		this.nom = nom;
-		this.force = force;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
+	   public Gaulois(String nom, int force) {
+	     this.nom = nom;
+	     this.force = force;
+	   }
+	   public String getNom() {
+	     return nom;
+	   }
+	   public void parler(String texte) {
+	     System.out.println(prendreParole() + "« " + texte + "»");
+	   }
+	   private String prendreParole() {
+	     return "Le gaulois " + nom + " : ";
+	   }
+	   public void frapper(Romain romain) {
+	     System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+	           + romain.getNom());
+	     romain.recevoirCoup(force / 3);
+	   }
+	   /*
+	   public String toString() {
+	     return "Gaulois [nom=" + nom + ", force=" + force
+	       + ", effetPotion=" + effetPotion + "]";
+	       }
+	   */
 	
-	public void parler(String texte) {
-		String parole = prendreparole();
-		System.out.println(parole + texte );
-		
+	   public static void main(String[] args) {
+	     asterix (nom = "astérix", force = 1);
+	     System.out.println(asterix);
+
+	     
+	   } 
 	}
-	
-	private String prendreparole() {
-		return "le Gaulois" + nom + ":"; 
-		
-	}
-	public static void main(String[] args) {
-		
-	}
-	
-}
+
 
 
