d909cc29158211b0134fcfc5950b453b994eddb4
flaw03 <100543813+flaw03@users.noreply.github.com>
2022-10-07 15:16:29 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,12 +3,16 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0; 
 	
 	public Romain(String nom, int force) {
 		super();
 		this.nom = nom;
 		this.force = force;
+		equipements = new Equipement[2];
 		assert force >0 ;
+		
 		
 	}
 	
@@ -28,17 +32,45 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
-		assert force < 0;
+		assert force > 0;
+		int f = force;
 		force -= forceCoup;
 		if (force > 0 ) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne ... ");
 		}
+		assert f > force;
+		
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+			case 0:
+				equipements[nbEquipement] = equipement;
+				nbEquipement+=1;
+				System.out.println("Le soldat "+nom+" s’équipe avec un " + equipement + ".");
+				break ;
+			case 1:
+				if (equipements[nbEquipement -1 ] == equipement){
+					System.out.println("Le soldat "+ nom + " possède deja un " + equipement +".");
+					}
+				else{
+					equipements[nbEquipement] = equipement ;
+					nbEquipement+=1;
+					System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement+ ".");
+				}
+				break;
+			default :
+				System.out.println("Le soldat "+ nom +" est déjà bien protégé !");
+		}
 	}
 	
 	public static void main(String[] args) {
-		Romain minus = new Romain("minus",5);
-	
+		Romain minus = new Romain("minus",6);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
